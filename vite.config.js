39fb17069744095ed8c9298375695ff03a17c3d2
mmubaidullah <ubaid111999@gmail.com>
2025-09-28 23:54:00 +0600

    Set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Customer-Support-Zone/",
+  base: "/A07-Customer-Support-Zone/",
 })
